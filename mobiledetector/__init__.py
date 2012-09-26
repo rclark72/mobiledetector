9e83f8c1f3d47f7d5a2598d4c062e3cb0404df9d
Robert Clark <robert@bablmedia.com>
2012-09-26 15:18:43 -0400

    simplified

--- mobiledetector/__init__.py
+++ mobiledetector/__init__.py
@@ -1,42 +1,29 @@
 from useragents import search_strings
-from webob import Request
 
-class Middleware(object):
-    def __init__(self, app):
-        """Adds a "mobile" attribute to the request which is True or False
-           depending on whether the request should be considered to come from a
-           small-screen device such as a phone or a PDA"""
-        self.app = app
+def is_mobile(request):
+    if hasattr(request, 'HTTP_X_OPERAMINI_FEATURES'):
+        # Then it's running opera mini. 'Nuff said.
+        # Reference from:
+        #  http://dev.opera.com/articles/view/opera-mini-request-headers/
+        return True
 
-    def __call__(self, environ, start_response):
-        request = Request(environ)
+    if hasattr(request, 'HTTP_ACCEPT'):
+        s = request.HTTP_ACCEPT.lower()
+        if 'application/vnd.wap.xhtml+xml' in s:
+            # Then it's a wap browser
+            return True
 
-        request.mobile = False
+    if hasattr(request, 'HTTP_USER_AGENT'):
+        # This takes the most processing. Surprisingly enough, when I
+        # Experimented on my own machine, this was the most efficient
+        # algorithm. Certainly more so than regexes.
+        # Also, Caching didn't help much, with real-world caches.
+        s = request.HTTP_USER_AGENT.lower()
+        for ua in search_strings:
+            if ua in s:
+                return True
+    return false
 
-        if hasattr(request, 'HTTP_X_OPERAMINI_FEATURES'):
-            # Then it's running opera mini. 'Nuff said.
-            # Reference from:
-            #  http://dev.opera.com/articles/view/opera-mini-request-headers/
-            request.mobile = True
-
-        if hasattr(request, 'HTTP_ACCEPT'):
-            s = request.HTTP_ACCEPT.lower()
-            if 'application/vnd.wap.xhtml+xml' in s:
-                # Then it's a wap browser
-                request.mobile = True
-
-        if hasattr(request, 'HTTP_USER_AGENT'):
-            # This takes the most processing. Surprisingly enough, when I
-            # Experimented on my own machine, this was the most efficient
-            # algorithm. Certainly more so than regexes.
-            # Also, Caching didn't help much, with real-world caches.
-            s = request.HTTP_USER_AGENT.lower()
-            for ua in search_strings:
-                if ua in s:
-                    request.mobile = True
-
-        resp = request.get_response(self.app)
-        return resp(environ, start_response)
 
 def detect_mobile(view):
     """View Decorator that adds a "mobile" attribute to the request which is
@@ -44,10 +31,10 @@ def detect_mobile(view):
        to come from a small-screen device such as a phone or a PDA"""
 
     def detected(request, *args, **kwargs):
-        Middleware.process_request(request)
+        request.mobile = is_mobile(request)
         return view(request, *args, **kwargs)
     detected.__doc__ = "%s\n[Wrapped by detect_mobile which detects if the request is from a phone]" % view.__doc__
     return detected
 
 
-__all__ = ['Middleware', 'detect_mobile']
+__all__ = ['detect_mobile']
